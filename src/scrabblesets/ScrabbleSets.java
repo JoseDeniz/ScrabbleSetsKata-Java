10e4e91d7b68383e7b4941f1413e4dadaef6b8cb
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2016-08-25 00:46:29 +0100

    Refactor: extract methods

--- src/scrabblesets/ScrabbleSets.java
+++ src/scrabblesets/ScrabbleSets.java
@@ -4,7 +4,6 @@ import java.util.Map;
 import java.util.stream.Stream;
 
 import static java.lang.String.format;
-import static java.lang.String.valueOf;
 import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.*;
 
@@ -16,28 +15,43 @@ class ScrabbleSets {
     }
 
     String tilesLeft(String input) {
-        Map<Character, Long> inputOccurrences = input
-                .chars()
-                .mapToObj(c -> (char) c)
-                .collect(groupingBy(identity(), counting()));
+        Map<Character, Long> remainingTiles = mergeMaps(input);
+        if (aTileHasTakenMoreThanPossible(remainingTiles)) {
+            return errorMessage();
+        }
+        return formatOutput(invertMap(remainingTiles));
+    }
 
-        Map<Character, Long> subtractValues = Stream.of(bag, inputOccurrences)
+    private Map<Character, Long> mergeMaps(final String input) {
+        return Stream.of(bag, groupByFrequency(input))
                 .flatMap(map -> map.entrySet().stream())
                 .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (val1, val2) -> val1 - val2));
+    }
 
-        if (subtractValues.entrySet().stream().anyMatch(e -> e.getValue() < 0)) {
-            return "Invalid input. More X's have been taken from the bag than possible.";
-        }
+    private Map<Character, Long> groupByFrequency(final String input) {
+        return input.chars()
+                    .mapToObj(c -> (char) c)
+                    .collect(groupingBy(identity(), counting()));
+    }
 
-        Map<Long, String> collect = subtractValues.entrySet().stream()
+    private boolean aTileHasTakenMoreThanPossible(Map<Character, Long> remainingTiles) {
+        return remainingTiles.entrySet().stream().anyMatch(e -> e.getValue() < 0);
+    }
+
+    private String errorMessage() {
+        return "Invalid input. More X's have been taken from the bag than possible.";
+    }
+
+    private Map<Long, String> invertMap(Map<Character, Long> remainingTiles) {
+        return remainingTiles.entrySet().stream()
                 .filter(entrySet -> entrySet.getValue() >= 0)
-                .collect(groupingBy(Map.Entry::getValue, mapping(e -> valueOf(e.getKey()), joining(", "))));
+                .collect(groupingBy(Map.Entry::getValue, mapping(e -> e.getKey().toString(), joining(", "))));
+    }
 
-        String formatEntries = collect.entrySet().stream()
+    private String formatOutput(Map<Long, String> collect) {
+        return collect.entrySet().stream()
                 .sorted((left, right) -> right.getKey().compareTo(left.getKey()))
                 .map(e -> format("%d: %s", e.getKey(), e.getValue()))
                 .collect(joining("\n"));
-
-        return formatEntries;
     }
 }
