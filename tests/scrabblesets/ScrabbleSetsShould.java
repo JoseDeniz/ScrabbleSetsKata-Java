af87a6c63e643f8a742d09172d921a37af009287
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2016-08-25 00:25:01 +0100

    Refactor: parametrized tests

--- tests/scrabblesets/ScrabbleSetsShould.java
+++ tests/scrabblesets/ScrabbleSetsShould.java
@@ -1,7 +1,10 @@
 package scrabblesets;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -9,12 +12,44 @@ import java.util.stream.Stream;
 
 import static java.lang.String.format;
 import static java.lang.String.valueOf;
+import static java.util.Arrays.asList;
 import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+@RunWith(Parameterized.class)
 public class ScrabbleSetsShould {
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return asList(new Object[][] {
+                { "PQAREIOURSTHGWIOAE_", "10: E\n" +
+                        "7: A, I\n" +
+                        "6: N, O\n" +
+                        "5: T\n" +
+                        "4: D, L, R\n" +
+                        "3: S, U\n" +
+                        "2: B, C, F, G, M, V, Y\n" +
+                        "1: H, J, K, P, W, X, Z, _\n" +
+                        "0: Q" },
+                {"LQTOONOEFFJZT", "11: E\n" +
+                        "9: A, I\n" +
+                        "6: R\n" +
+                        "5: N, O\n" +
+                        "4: D, S, T, U\n" +
+                        "3: G, L\n" +
+                        "2: B, C, H, M, P, V, W, Y, _\n" +
+                        "1: K, X\n" +
+                        "0: F, J, Q, Z"},
+                {"AXHDRUIOR_XHJZUQEE", "Invalid input. More X's have been taken from the bag than possible."},
+        });
+    }
+
+    @Parameterized.Parameter
+    public String input;
+    @Parameterized.Parameter(value = 1)
+    public String expectedOutput;
 
     private static final Map<Character, Long> bag = new HashMap<>();
     static {
@@ -49,29 +84,7 @@ public class ScrabbleSetsShould {
 
     @Test
     public void return_the_remaining_tiles_in_the_bag() {
-        String expected1 = "10: E\n" +
-                           "7: A, I\n" +
-                           "6: N, O\n" +
-                           "5: T\n" +
-                           "4: D, L, R\n" +
-                           "3: S, U\n" +
-                           "2: B, C, F, G, M, V, Y\n" +
-                           "1: H, J, K, P, W, X, Z, _\n" +
-                           "0: Q";
-        String expected2 = "11: E\n" +
-                            "9: A, I\n" +
-                            "6: R\n" +
-                            "5: N, O\n" +
-                            "4: D, S, T, U\n" +
-                            "3: G, L\n" +
-                            "2: B, C, H, M, P, V, W, Y, _\n" +
-                            "1: K, X\n" +
-                            "0: F, J, Q, Z";
-        String expected3 = "Invalid input. More X's have been taken from the bag than possible.";
-
-        assertThat(tilesLeft("PQAREIOURSTHGWIOAE_"), is(expected1));
-        assertThat(tilesLeft("LQTOONOEFFJZT"), is(expected2));
-        assertThat(tilesLeft("AXHDRUIOR_XHJZUQEE"), is(expected3));
+        assertThat(tilesLeft(input), is(expectedOutput));
     }
 
     private String tilesLeft(String input) {
